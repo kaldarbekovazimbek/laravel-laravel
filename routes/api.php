307d8c74e9a73930c4cdbb7e558d5a3c1fa8a1d6
Azimbek <azimbek.kaldarbekov@alif.uz>
2024-06-16 16:55:19 +0500

    2nd commit

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +18,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', [UserController::class,'index']);
